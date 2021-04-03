963ae595ae8d60aa2466af8203d51983b22ce3c3
Ayelet Danieli <ayeletd@gmail.com>
2021-04-03 04:39:28 +0300

    firebase setup and signup component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-// import { pets } from "./assets/img/pets-1000x413.png";
 import "./App.css";
-// import { findRenderedComponentWithType } from "react-dom/test-utils";
+import Signup from "./Signup";
 
 function App() {
   return (
     <div className="App">
       <div className="under-construction" alt="under construction">
         <h1>hi, I am under construction</h1>
+        <Signup />
       </div>
     </div>
   );
