0b7aa15a0c6ee7a9de20eb55f2263b743d0165f2
Ayelet Danieli <ayeletd@gmail.com>
2021-04-07 11:41:34 +0300

    added cartProvider to all children

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import CartProvider from "./CartContext/CartContext";
 // import Signup from "./UserLogin/signup";
 // import { Container } from "react-bootstrap";
 import IndexPage from "./Pages/";
@@ -8,7 +9,9 @@ import IndexPage from "./Pages/";
 function App() {
   return (
     <BrowserRouter>
-      <IndexPage />{" "}
+      <CartProvider>
+        <IndexPage />{" "}
+      </CartProvider>
     </BrowserRouter>
   );
 }
