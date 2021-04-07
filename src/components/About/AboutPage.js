081aa6d64af1d52cc04e51aff83dac7d110543e3
Ayelet Danieli <ayeletd@gmail.com>
2021-04-07 13:06:02 +0300

    about page

--- src/components/About/AboutPage.js
+++ src/components/About/AboutPage.js
@@ -2,45 +2,27 @@ import React from "react";
 // import HeroPage from "../HeroPage/HeroPage";
 // import Layout from "../Layout/Layout";
 import "../Layout/Layout.css";
-// import AboutImg from "../../assets/img/about.jpg";
-import { Container, Row, Col } from "react-bootstrap";
+import "./About.css";
+// import AboutImg from "../../assets/img/about.webp";
+import { Row } from "react-bootstrap";
 
 export default function AboutPage() {
   return (
-    <Container>
+    <div>
       <Row className="about">
-        <Col></Col>
-        <Col className="text-white text-center mt-4">
-          <h3>About Us</h3>
-          <p className="width-50">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque
-            quis, facilis libero minima explicabo placeat dignissimos neque
-            harum ducimus deserunt dolorum quas labore, unde fugiat assumenda
-            pariatur ipsam, esse fugit. Perferendis consectetur, fugiat rerum
-            facilis dignissimos adipisci, quas mollitia vitae numquam,
-            repellendus hic nemo error pariatur officiis. Alias, fuga
-            accusantium, quis suscipit vel ea optio hic dignissimos recusandae
-            cum reiciendis aperiam. Inventore perspiciatis accusamus autem
-            fugiat aut reprehenderit sunt accusantium repellendus praesentium
-            minima repudiandae adipisci hic dolor aliquam debitis amet error
-            maiores placeat quisquam natus ipsam voluptatibus, quis distinctio.
-            Illum quod sed tenetur assumenda eum earum animi? Necessitatibus
-            esse at aspernatur ex eligendi odio ea maxime exercitationem modi
-            nobis, quasi accusamus itaque amet. Possimus animi et eum corporis
-            sint adipisci quisquam mollitia soluta, sed fuga quaerat saepe
-            accusantium temporibus voluptates dolor consectetur voluptate
-            impedit praesentium aspernatur magni laborum maiores aliquid. Atque,
-            neque ipsum fuga repudiandae voluptas nulla doloremque tenetur
-            accusamus, quae assumenda fugit deleniti delectus tempora quam rem
-            nobis, consectetur officiis. Maiores, iste nam numquam veritatis
-            voluptas vitae aut sequi quisquam accusamus error id animi,
-            repellendus facere recusandae necessitatibus. Laborum reiciendis
-            tempore aut ex veritatis tempora reprehenderit vero nam quia et
-            alias repudiandae unde, ullam vel necessitatibus culpa molestias
-            dolorum!
+        <div className="about--img col-md-6"></div>
+        <div className="about--info col-md-6 product-detail-card-text">
+          <h1>About Us</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+            aspernatur culpa repellendus aut aliquam, consectetur laborum
+            consequuntur earum? Mollitia tempore omnis beatae vero eveniet harum
+            perferendis aliquid laudantium nulla soluta odit alias iusto ipsa
+            non, natus fugit est atque. Sed corrupti, velit esse ratione facere
+            aperiam rem assumenda beatae autem?
           </p>
-        </Col>
+        </div>
       </Row>
-    </Container>
+    </div>
   );
 }
