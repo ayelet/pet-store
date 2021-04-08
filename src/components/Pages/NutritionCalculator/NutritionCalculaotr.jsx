352aabedebe19c56959cca4c79e2389ec97b710e
Ayelet Danieli <ayeletd@gmail.com>
2021-04-08 05:57:41 +0300

    Added Nutrition Calculator Mock Page

--- src/components/Pages/NutritionCalculator/NutritionCalculaotr.jsx
+++ src/components/Pages/NutritionCalculator/NutritionCalculaotr.jsx
@@ -1,8 +1,137 @@
 import React from "react";
+import "./NutritionCalculator.css";
 
 const NutritionCalculaotr = () => {
+  // return (
+  //   <div className="under-construction">This site is under construction</div>
+  // );
   return (
-    <div className="under-construction">This site is under construction</div>
+    <div class="component-content" id="1999618036">
+      <div class="paragraphSystem content">
+        <a href="/#" id="02092368546" style={{ visibility: "hidden" }}>
+          ""
+        </a>
+        <div class="richText component section default-style first odd col-xs-12">
+          <div class="component-content">
+            <div class="richText-content">
+              <h3>
+                We offer Diet foods & clinically tested, therapeutic nutrition
+                for a range of conditions unique to each pet.
+              </h3>
+            </div>
+          </div>
+        </div>
+        <div class="image component section default-style even col-xs-12">
+          <div class="component-content left">
+            <div class="analytics-image-tracking"></div>
+            <figure>
+              <picture>
+                <img
+                  src="https://www.hillspet.com/content/dam/cp-sites/hills/hills-pet/global/prescription-diet-redesign/icon-urinary-health.png"
+                  alt="ICON URINARY HEALTH"
+                />
+              </picture>
+
+              <figcaption>
+                <h4>URINARY HEALTH</h4>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="image component section default-style odd col-xs-12">
+          <div class="component-content left">
+            <div class="analytics-image-tracking"></div>
+            <figure>
+              <picture>
+                <img
+                  src="https://www.hillspet.com/content/dam/cp-sites/hills/hills-pet/global/prescription-diet-redesign/icon-weight-management.png"
+                  alt="ICON WEIGHT MANAGEMENT"
+                />
+              </picture>
+
+              <figcaption>
+                <h4>WEIGHT MANAGEMENT</h4>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="image component section default-style even col-xs-12">
+          <div class="component-content left">
+            <div class="analytics-image-tracking"></div>
+            <figure>
+              <picture>
+                <img
+                  src="https://www.hillspet.com/content/dam/cp-sites/hills/hills-pet/global/prescription-diet-redesign/icon-kidney-support.png"
+                  alt="ICON KIDNEY SUPPORT"
+                />
+              </picture>
+
+              <figcaption>
+                <h4>KIDNEY SUPPORT</h4>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="image component section default-style odd col-xs-12">
+          <div class="component-content left">
+            <div class="analytics-image-tracking"></div>
+            <figure>
+              <picture>
+                <img
+                  src="https://www.hillspet.com/content/dam/cp-sites/hills/hills-pet/global/prescription-diet-redesign/icon-digestive-care.png"
+                  alt="ICON DIGESTIVE CARE"
+                />
+              </picture>
+
+              <figcaption>
+                <h4>DIGESTIVE CARE</h4>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="image component section default-style even col-xs-12">
+          <div class="component-content left">
+            <div class="analytics-image-tracking"></div>
+            <figure>
+              <picture>
+                <img
+                  src="https://www.hillspet.com/content/dam/cp-sites/hills/hills-pet/global/prescription-diet-redesign/icon-skin-coat.png"
+                  alt="ICON SKIN &amp; COAT HEALTH"
+                />
+              </picture>
+
+              <figcaption>
+                <h4>SKIN &amp;&nbsp;COAT HEALTH</h4>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+        <div class="richText component section default-style odd last col-xs-12">
+          <div class="component-content">
+            <div class="richText-content">
+              <p>
+                These are just a few of the health conditions Prescription Diet
+                products cover — browse our full offerings below.
+              </p>
+              <p style={{ textAlign: "center" }}>
+                <a
+                  href="https://www.hillspet.com/products/dog-food#pd"
+                  class="blue-cta-button"
+                >
+                  Browse Dog Food
+                </a>{" "}
+                <a
+                  href="https://www.hillspet.com/products/cat-food#pd"
+                  class="blue-cta-button"
+                >
+                  Browse Cat Food
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
